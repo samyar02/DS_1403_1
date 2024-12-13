750e58118b4a14efabe663b7819e272344aad0e4
samyar02 <152595241+samyar02@users.noreply.github.com>
2024-12-13 10:32:59 +0330

    used stack method

--- comparing lenghth in array of columns.py
+++ comparing lenghth in array of columns.py
@@ -1,66 +1,41 @@
 import math
+
 class Tower:
     def __init__(self, a, b, c):
-        self.a = a  # Height of the tower
-        self.b = b  # Resistance of the tower
-        self.c = c  # Power of the tower
-        self.left = 0  # Cumulative power from left
-        self.right = 0  # Cumulative power from right
+        self.a = a  
+        self.b = b  
+        self.c = c  
+        self.left = 0 
+        self.right = 0 
 
-def find_left(towers, i, b):
-    """
-    Find the left value of the first tower j (where j < i)
-    such that tower[j].a >= tower[i].a.
-    """
-    for j in range(i - 1, -1, -1):  # Look at all previous towers
-        if towers[j].a >= b:
-            return towers[j].left
-    return 0  # Return 0 if no such tower is found
+n = int(input()) 
+output = []  
 
-def find_right(towers, i, m):
-    """
-    Find the right value of the first tower j (where j > i)
-    such that tower[j].a >= tower[i].a.
-    """
-    for j in range(i + 1, m):  # Look at all subsequent towers
-        if towers[j].a >= towers[i].a:
-            return towers[j].right
-    return 0  # Return 0 if no such tower is found
-
-
-n = int(input())  # Number of scenarios
 for _ in range(n):
-    m = int(input())  # Number of towers in the scenario
+    m = int(input()) 
     towers = []
-    buff = 0
-    preA = 0
-
-    # Calculate the `left` values
+    stack = []
+    leftsum = 0
     for i in range(m):
-        line = input()
-        values = list(map(int, line.split()))
-        tower = Tower(*values)
-        if preA >= tower.a:
-            buff += tower.c 
-        else:
-            buff = tower.c + find_left(towers, i, tower.a)
-        tower.left = buff
-        preA = tower.a
+        a, b, c = map(int, input().split())
+        tower = Tower(a, b, c)
+        tower.left = leftsum
+        while stack and stack[-1].a < tower.a:
+            leftsum -= stack.pop().c
+        stack.append(tower)
+        leftsum += tower.c
         towers.append(tower)
 
-    buff = 0
-    preA = 0
-    result = [] 
+    stack = [] 
+    righsum = 0
+    result = []
     for i in range(m - 1, -1, -1):
         tower = towers[i]
-        if preA >= tower.a:
-            buff += tower.c
-        else:
-            buff = tower.c + find_right(towers, i, m)
-        tower.right = buff
-        preA = tower.a
-        left_val = towers[i - 1].left if i > 0 else 0  
-        right_val = towers[i + 1].right if i < m - 1 else 0 
-        result.append(math.ceil( tower.b / (left_val + right_val)))
-
-    print(" ".join(map(str, result[::-1])))
+        tower.right = righsum
+        while stack and stack[-1].a < tower.a:
+            righsum -= stack.pop().c
+        stack.append(tower)
+        righsum += tower.c
+        result.append(math.ceil(tower.b / (tower.left + tower.right)))
+    output.append(" ".join(map(str, result[::-1])))
+print("\n".join(output))
